4102e94a4b9c1918f790b0a2bb78acf0db96cb3e
Tomek Puzio <tomekpdevelopment@gmail.com>
2022-01-31 20:47:20 +0100

    add attack and response

---
+++ app.js
@@ -0,0 +1,26 @@
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        };
+    },
+    methods: {
+        attackMonster() {
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+            console.log(this.monsterHealth)
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        }
+    },
+});
+
+app.mount('#game');
